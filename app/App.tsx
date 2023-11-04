49ba6e0b4fb8b2d71228f1c5748436670e38496c
bibekacharya-123 <bacharya1397@gmail.com>
2023-11-04 13:32:49 +0545

    a

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 export default function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-        
+        <View>
+          <Register/>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
