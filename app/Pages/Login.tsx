8d72c4028a97fd90ba14a58fef77c5438f2e4075
bibekacharya-123 <bacharya1397@gmail.com>
2023-11-04 13:38:49 +0545

    b

--- app/Pages/Login.tsx
+++ app/Pages/Login.tsx
@@ -1,77 +1,58 @@
-import { Text, StyleSheet, View, StatusBar, TextInput, TouchableOpacity, ScrollView,SafeAreaView } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View, StatusBar, TextInput, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
+import React, { Component, useState } from 'react'
 
 
-import MAt from 'react-native-vector-icons/MaterialCommunityIcons'
+import Arrowleft from 'react-native-vector-icons/AntDesign'
 
-import { useState } from 'react'
-export default class Login extends Component {
-
-
+export default class Register extends Component {
   render() {
-    const [isChecked, setisChecked] = useState(false);
 
     return (
       <SafeAreaView style={styles.all}>
         <ScrollView>
-          <StatusBar backgroundColor="#690c23" barStyle="light-content" />
+          <StatusBar backgroundColor="white" barStyle="dark-content" />
           <View style={styles.header}>
             <TouchableOpacity>
-              <MAt name='arrow-left-bold-box' size={30} color="#ffff" />
+              < Arrowleft name='arrowleft' size={30} color="#ffff" />
+
             </TouchableOpacity>
-            <Text style={styles.headerText}>  </Text>
+            <Text style={{fontSize:25,color:'black'}}>Login</Text>
+
           </View>
           <View style={styles.container}>
-            <View style={{ alignItems: 'center' }}>
-              <Text style={styles.texttt}>Club MBT Login</Text>
+           
+
+          
+            <View>
+
+              <TextInput
+                style={styles.textinput}
+                placeholder='Input Email Address' />
             </View>
-            <View style={{ alignItems: 'center' }}>
-              <Text style={styles.textt}>Access the dashboard and join the club</Text>
+            <View>
+
+              <TextInput
+                style={styles.textinput}
+                placeholder='password'
+
+                autoCapitalize='none'
+                autoCorrect={false}
+                secureTextEntry={true}  />
             </View>
 
-            <View style={{padding:5}}>
-              <View>
-
-                <TextInput
-                  style={styles.textinput}
-                  placeholder='Mobile Number or Email*' />
-              </View>
-              <View>
-
-                <TextInput
-                  style={styles.textinput}
-                  placeholder='Password*'
-                  secureTextEntry={true} />
-              </View>
-            </View>
-            <View style={{flexDirection: 'row',justifyContent:'space-between',margin:10,marginTop:1,marginBottom:2}}>
-              <View>
-                
-                <Text style={{fontSize:18}}> Remember Me</Text>
-              </View>  
-              <Text style={{fontSize:18}}>
-                Forgot Password?
-              </Text>
-            </View>
-            <TouchableOpacity>
-            <View style={styles.log}>
-            
-            <Text style={{ color: "#ffff", fontSize: 23, fontFamily: 'roboto' }}>Login</Text>
-         
-        </View>
+            <TouchableOpacity style={styles.log}>
+              <Text style={{ color: "#ffff", fontSize: 18, fontFamily: 'roboto' }}>LOGIN</Text>
             </TouchableOpacity>
-            <View style={styles.loginwith}>
+            <View>
 
               <TouchableOpacity>
-                <Text style={{ color: '#black', fontSize: 20, fontFamily: 'roboto' }}>Register</Text>
+               <View style={styles.ref}>
+               <Text style={{ color: 'blue', fontSize: 15, fontFamily: 'roboto',fontWeight:'bold'}}>REGISTER NOW!</Text>
+                <Text style={{ color: 'blue', fontSize: 15, fontFamily: 'roboto' ,fontWeight:'bold'}}>fORGOT PASSWORD?</Text>
+               </View>
               </TouchableOpacity>
             </View>
-            <View style={styles.fgt}>
-              <View >
 
-              </View>
-
-            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -81,10 +62,10 @@ export default class Login extends Component {
 
 const styles = StyleSheet.create({
   all: {
-    backgroundColor: "#690c23"
+    backgroundColor: "#fffff"
   },
   header: {
-    backgroundColor: '#690c23',
+    backgroundColor: '#ffffff',
     padding: 20,
     flexDirection: 'row'
   },
@@ -95,54 +76,40 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#EEEEEE',
+    backgroundColor: '#ffffff',
     padding: 16,
     borderTopLeftRadius: 16,
     borderTopRightRadius: 16,
     height: 780
   },
   textinput: {
-    fontSize: 18,
+    fontSize: 15,
     borderWidth: 2,
-    borderColor: '#690c23',
-    borderRadius: 10,
+    borderColor: 'white',
+    borderRadius: 8,
     padding: 10,
-    margin: 5
+    margin: 5,
+    backgroundColor:'lightgrey'
 
   },
   log: {
     height: 50,
-    width: '97%',
-    backgroundColor: '#690c23',
+    width: '96%',
+    backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 20,
-    margin: 8,
-    marginRight: 20,
-    paddingleft: 10,
-    paddingRight: 10,
+    borderRadius: 10,
+    marginLeft:6,
+  
+
 
   },
-  loginwith: {
-
-    padding: 10,
-    alignItems: 'center',
-
-  },
-  fgt: {
-
-  },
-  texttt: {
-    fontSize: 20,
-    color: '#690c23',
-    fontFamily: 'roboto',
-    fontWeight: 'bold',
-  },
-  textt: {
-    fontSize: 15,
-    color: 'grey',
-    fontFamily: 'roboto',
-    fontWeight: 'bold',
+  ref:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    
+    padding:5
   }
+
 
 })
--- app/Pages/Register.tsx
+++ app/Pages/Register.tsx
@@ -1,115 +1,12 @@
-import { Text, StyleSheet, View, StatusBar, TextInput, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
-import React, { Component, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-
-import Arrowleft from 'react-native-vector-icons/AntDesign'
-
-export default class Register extends Component {
-  render() {
-
-    return (
-      <SafeAreaView style={styles.all}>
-        <ScrollView>
-          <StatusBar backgroundColor="white" barStyle="dark-content" />
-          <View style={styles.header}>
-            <TouchableOpacity>
-              < Arrowleft name='arrowleft' size={30} color="#ffff" />
-
-            </TouchableOpacity>
-            <Text style={{fontSize:25,color:'black'}}>Login</Text>
-
-          </View>
-          <View style={styles.container}>
-           
-
-          
-            <View>
-
-              <TextInput
-                style={styles.textinput}
-                placeholder='Input Email Address' />
-            </View>
-            <View>
-
-              <TextInput
-                style={styles.textinput}
-                placeholder='password'
-
-                autoCapitalize='none'
-                autoCorrect={false}
-                secureTextEntry={true}  />
-            </View>
-
-            <TouchableOpacity style={styles.log}>
-              <Text style={{ color: "#ffff", fontSize: 18, fontFamily: 'roboto' }}>LOGIN</Text>
-            </TouchableOpacity>
-            <View>
-
-              <TouchableOpacity>
-               <View style={styles.ref}>
-               <Text style={{ color: 'blue', fontSize: 15, fontFamily: 'roboto',fontWeight:'bold'}}>REGISTER NOW!</Text>
-                <Text style={{ color: 'blue', fontSize: 15, fontFamily: 'roboto' ,fontWeight:'bold'}}>fORGOT PASSWORD?</Text>
-               </View>
-              </TouchableOpacity>
-            </View>
-
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    )
-  }
+export default function Register() {
+  return (
+    <View>
+      <Text>Register</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  all: {
-    backgroundColor: "#fffff"
-  },
-  header: {
-    backgroundColor: '#ffffff',
-    padding: 20,
-    flexDirection: 'row'
-  },
-  headerText: {
-    fontSize: 23,
-    fontWeight: 'bold',
-    color: '#ffff'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    padding: 16,
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    height: 780
-  },
-  textinput: {
-    fontSize: 15,
-    borderWidth: 2,
-    borderColor: 'white',
-    borderRadius: 8,
-    padding: 10,
-    margin: 5,
-    backgroundColor:'lightgrey'
-
-  },
-  log: {
-    height: 50,
-    width: '96%',
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-    marginLeft:6,
-  
-
-
-  },
-  ref:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    
-    padding:5
-  }
-
-
-})
+const styles = StyleSheet.create({})
