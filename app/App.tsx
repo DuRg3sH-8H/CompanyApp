e1a6acd4e9fd672da3a2f99b82bfb30620b8356c
bibekacharya-123 <bacharya1397@gmail.com>
2023-11-25 13:42:27 +0545

    l

--- app/App.tsx
+++ app/App.tsx
@@ -3,21 +3,14 @@ import React from 'react'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
 import Forgotpassword from './Pages/ForgotPassword'
-<<<<<<< HEAD
-import ContactUs from './Pages/ContactUs'
-=======
 import More from './Pages/More'
->>>>>>> a53ce613be974e8c2c4a54161b10828b2aa5d60f
+import ContactUs from './Pages/ContactUs'
 
 export default function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-<<<<<<< HEAD
         <ContactUs/>
-=======
-        <More/>
->>>>>>> a53ce613be974e8c2c4a54161b10828b2aa5d60f
       </ScrollView>
     </SafeAreaView>
   )
